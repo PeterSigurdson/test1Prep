8f8dd3731fb20aa474929df0d318ec84f35d903e
peter sigurdson <peter@petersigurdson.net>
2019-02-10 13:48:35 -0500

    Test 1 Sample Code

--- test1Prep/Program.cs
+++ test1Prep/Program.cs
@@ -6,7 +6,31 @@ namespace test1Prep
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Example1.run();
+        }
+    }
+
+    class Example1
+    {
+        static int i = 0;
+
+        public static void run()
+        {
+            
+            for (; MethodA() ; )
+            {
+                Console.WriteLine(i--);
+                if ( i < -11) { break;}
+            }
+            // point A:
+            Console.WriteLine("I am after Point A");
+            Console.WriteLine("i is {0} ", i);
+        }
+
+        public static bool MethodA()
+        {
+            i -= 1;
+            return true;
         }
     }
 }
