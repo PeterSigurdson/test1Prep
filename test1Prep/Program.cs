dfbd759f4a0b36c240580e51f8f12b3bd847c45d
peter sigurdson <peter@petersigurdson.net>
2019-02-10 13:16:59 -0500

    Add project files.

---
+++ test1Prep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test1Prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
